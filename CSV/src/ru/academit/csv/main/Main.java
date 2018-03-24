bb49d9e9b89c8e2840398a211435bbf1e36ee202
Your Name <qeadws12>
2018-03-24 16:58:57 +0700

    CSV

---
+++ CSV/src/ru/academit/csv/main/Main.java
@@ -0,0 +1,35 @@
+package ru.academit.csv.main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String inputPath = "CSV\\src\\table.csv";
+        String outputPath = "CSV\\src\\table.html";
+        String delimiter = ";";
+
+        try (Scanner reader = new Scanner(new FileInputStream(inputPath), "windows-1251");
+             PrintWriter writer = new PrintWriter(outputPath)) {
+
+            writer.append("<table border=\"1\" width=\"100%\">");
+            while (reader.hasNextLine()) {
+                writer.append("<tr>");
+
+              String[] strings = reader.nextLine().replace("<", "&lt").replace(">", "&gt").replace("&", "&amp").split(delimiter);
+
+                for (String string : strings) {
+                    writer.append("<td><center>");
+                    writer.append(string);
+                    writer.append("</center></td>");
+                }
+                writer.append("<br></tr>");
+            }
+            writer.append("</table>");
+        }
+    }
+}
