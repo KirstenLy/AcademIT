c090d6c7c7b6236132dc6fe1d843e3f6591819e3
Your Name <qeadws12>
2018-03-27 03:21:17 +0700

    Заметка для себя: Шляпа не рабочая. Продумать как научить программу определить где правильно расставлять <tr></tr> для организации таблицы.

--- CSV/src/ru/academit/csv/main/Main.java
+++ CSV/src/ru/academit/csv/main/Main.java
@@ -2,35 +2,80 @@ package ru.academit.csv.main;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
         String inputPath = "CSV\\src\\table.csv";
         String outputPath = "CSV\\src\\table.html";
-        String delimiter = ";";
+        StringBuilder temp = new StringBuilder();
+        StringBuilder stringBuilder = new StringBuilder();
+        char delimiter = ';';
+        char lastSymbolInLine = ';';
 
-        try (Scanner reader = new Scanner(new FileInputStream(inputPath), "windows-1251");
-             PrintWriter writer = new PrintWriter(outputPath)) {
-
-            writer.append("<table border=\"1\" width=\"100%\">");
-            while (reader.hasNextLine()) {
-                writer.append("<tr>");
-
-                String[] strings = reader.nextLine().replace("&", "&amp").replace("<", "&lt").replace(">", "&gt").split(delimiter);
-
-                for (String string : strings) {
-                    writer.append("<td><center>");
-                    writer.append(string);
-                    writer.append("</center></td>");
+        while (true) {
+            try (Scanner reader = new Scanner(new FileInputStream(inputPath), "windows-1251");
+                 PrintWriter writer = new PrintWriter(outputPath)) {
+                if (!reader.hasNext()) {
+                    throw new IllegalArgumentException("Исходный фаил пуст.");
                 }
-                writer.append("<br></tr>");
+
+                writer.append("<!DOCTYPE HTML>\n<html>\n");
+                writer.append("<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n</head>\n");
+                writer.append("<title>CSV</title>\n");
+                writer.append("<body>\n");
+                writer.append("<table border=\"1\" width=\"100%\">\n");
+
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
+                    if (line.isEmpty()) {
+                        continue;
+                    }
+                    char[] charsFromLine = line.toCharArray();
+                    if (lastSymbolInLine == delimiter) {
+                        writer.append("<tr>\n");
+                    }
+                    lastSymbolInLine = charsFromLine[charsFromLine.length - 1];
+
+                    for (int i = 0; i < charsFromLine.length; i++) {
+                        if (charsFromLine[i] == delimiter) {
+                            writer.append("<td><center>").append(temp).append(stringBuilder.toString()).append("</center></td>\n");
+                            stringBuilder.setLength(0);
+                        } else if (charsFromLine[i] == '<') {
+                            stringBuilder.append("&lt;");
+                        } else if (charsFromLine[i] == '>') {
+                            stringBuilder.append("&gt;");
+                        } else if (charsFromLine[i] == '&') {
+                            stringBuilder.append("&amp;");
+                        } else if (i == charsFromLine.length - 1) {
+                            stringBuilder.append(charsFromLine[i]);
+                            stringBuilder.append("<br>");
+                        } else {
+                            stringBuilder.append(charsFromLine[i]);
+                        }
+                    }
+                }
+
+                if (lastSymbolInLine == delimiter) {
+                    writer.append("</tr>\n");
+                }
+
+                writer.append("</tr></table>\n</body>\n</html>");
+                System.out.print("Done.");
+                return;
+            } catch (FileNotFoundException e) {
+                    System.out.println("Исходный фаил не найден. Укажите корректный путь к CSV таблице:");
+                    inputPath = new Scanner(System.in).nextLine();
             }
-            writer.append("</table>");
-            System.out.print("Done.");
+            catch (IllegalArgumentException e){
+                System.out.println("Исходный фаил пуст. Укажите корректный путь к CSV таблице:");
+                inputPath = new Scanner(System.in).nextLine();
+            }
         }
     }
 }
+
