fd89aea623f22045b5fedfc8faa148f1aa0be475
Your Name <qeadws12>
2018-04-11 18:41:59 +0700

    ...

--- CSV/src/ru/academit/csv/main/Main.java
+++ CSV/src/ru/academit/csv/main/Main.java
@@ -1,9 +1,6 @@
 package ru.academit.csv.main;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -13,15 +10,15 @@ public class Main {
         String inputPath = "CSV\\src\\table.csv";
         String outputPath = "CSV\\src\\table.html";
         StringBuilder resultString = new StringBuilder();
-        char delimiter = ';';
-        boolean flag = true;
-        boolean flag1 = false;
+        char delimiter = ',';
+        boolean isInQuotes = false;
 
         while (true) {
             try (Scanner reader = new Scanner(new FileInputStream(inputPath), "windows-1251");
                  PrintWriter writer = new PrintWriter(outputPath)) {
+
                 if (!reader.hasNext()) {
-                    throw new IllegalArgumentException("Исходный фаил пуст.");
+                    throw new IllegalArgumentException("Исходный фаил пуст. Укажите корректный путь к CSV таблице:");
                 }
 
                 writer.append("<!DOCTYPE HTML>\n<html>\n");
@@ -31,39 +28,40 @@ public class Main {
                 writer.append("<table border=\"1\" width=\"100%\">\n");
 
                 while (reader.hasNextLine()) {
-                    char[] lineArray = reader.nextLine().toCharArray();
-                    int lastIndexDelimiter = 0;
 
-                    if (!flag1) {
+                    if (!isInQuotes) {
                         writer.append("<tr>\n");
                     }
 
-                    for (int i = 0; i < lineArray.length; i++) {
-                        if (lineArray[i] == delimiter) {
-                            lastIndexDelimiter = i;
-                            if (flag1) {
-                                writer.append(resultString.toString()).append("</td>");
-                                resultString.setLength(0);
-                                flag1 = false;
-                            } else {
-                                writer.append("<td>").append(resultString.toString()).append("</td>");
-                                resultString.setLength(0);
-                            }
-                        } else if (i == lineArray.length - 1 & lineArray[lastIndexDelimiter + 1] != '"') {
-                            resultString.append(lineArray[i]);
-                            writer.append("<td>").append(resultString.toString()).append("</td>");
-                            resultString.setLength(0);
-                            writer.append("</tr>");
-                        } else if (i == lineArray.length - 1 & lineArray[lastIndexDelimiter + 1] == '"') {
-                            resultString.append("<td>").append(lineArray[i]).append("<br>");
+                    char[] charArrayFromLine = reader.nextLine().toCharArray();
 
+                    for (int i = 0; i < charArrayFromLine.length; i++) {
+                        if (charArrayFromLine[i] == '"') {
+                            isInQuotes = !isInQuotes;
+                        } else if (charArrayFromLine[i] == delimiter) {
+                            resultString.append("");
+                        } else if (charArrayFromLine[i] == '<') {
+                            resultString.append("&lt;");
+                        } else if (charArrayFromLine[i] == '>') {
+                            resultString.append("&gt;");
+                        } else if (charArrayFromLine[i] == '&') {
+                            resultString.append("&amp;");
                         } else {
-                            resultString.append(lineArray[i]);
+                            resultString.append(charArrayFromLine[i]);
+                        }
+
+                        if (!isInQuotes & (charArrayFromLine[i] == delimiter | (i == charArrayFromLine.length - 1 & !isInQuotes))) {
+                            writer.append("<td><center>").append(resultString.toString()).append("</center></td>");
+                            resultString.setLength(0);
+                        }
+
+                        if (isInQuotes & i == charArrayFromLine.length - 1) {
+                            resultString.append("<br>");
                         }
                     }
-                    flag1 = lineArray[lastIndexDelimiter + 1] == '"';
-                    if (!flag1) {
-                        writer.append("\n</tr>\n");
+
+                    if (!isInQuotes) {
+                        writer.append("</tr>\n");
                     }
                 }
 
@@ -74,37 +72,9 @@ public class Main {
                 System.out.println("Исходный фаил не найден. Укажите корректный путь к CSV таблице:");
                 inputPath = new Scanner(System.in).nextLine();
             } catch (IllegalArgumentException e) {
-                System.out.println("Исходный фаил пуст. Укажите корректный путь к CSV таблице:");
+                System.out.println(e.getMessage());
                 inputPath = new Scanner(System.in).nextLine();
             }
         }
     }
 }
-
-//    String line = reader.nextLine();
-//                    if (line.isEmpty()) {
-//                            continue;
-//                            }
-//                            char[] charsFromLine = line.toCharArray();
-//                            if (lastSymbolInLine == delimiter) {
-//                            writer.append("<tr>\n");
-//                            }
-//                            lastSymbolInLine = charsFromLine[charsFromLine.length - 1];
-//
-//                            for (int i = 0; i < charsFromLine.length; i++) {
-//        if (charsFromLine[i] == delimiter) {
-//        writer.append("<td><center>").append(temp).append(stringBuilder.toString()).append("</center></td>\n");
-//        stringBuilder.setLength(0);
-//        } else if (charsFromLine[i] == '<') {
-//        stringBuilder.append("&lt;");
-//        } else if (charsFromLine[i] == '>') {
-//        stringBuilder.append("&gt;");
-//        } else if (charsFromLine[i] == '&') {
-//        stringBuilder.append("&amp;");
-//        } else if (i == charsFromLine.length - 1) {
-//        stringBuilder.append(charsFromLine[i]);
-//        stringBuilder.append("<br>");
-//        } else {
-//        stringBuilder.append(charsFromLine[i]);
-//        }
-//        }
