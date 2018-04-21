8f19cfce0e2686e14ff8196d996a2a7fbd286bcf
Your Name <qeadws12>
2018-04-21 22:36:06 +0700

    Работает, но только если в таблице нет "", иначе всё наперекосяк идёт. Придумать что сделать с """"";

--- CSV/src/ru/academit/csv/main/Main.java
+++ CSV/src/ru/academit/csv/main/Main.java
@@ -13,68 +13,59 @@ public class Main {
         char delimiter = ',';
         boolean isInQuotes = false;
 
-        while (true) {
-            try (Scanner reader = new Scanner(new FileInputStream(inputPath), "windows-1251");
-                 PrintWriter writer = new PrintWriter(outputPath)) {
+        try (Scanner reader = new Scanner(new FileInputStream(inputPath), "windows-1251");
+             PrintWriter writer = new PrintWriter(outputPath)) {
 
-                if (!reader.hasNext()) {
-                    throw new IllegalArgumentException("Исходный фаил пуст. Укажите корректный путь к CSV таблице:");
-                }
-
-                writer.append("<!DOCTYPE HTML>\n<html>\n");
-                writer.append("<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n</head>\n");
-                writer.append("<title>CSV</title>\n");
-                writer.append("<body>\n");
-                writer.append("<table border=\"1\" width=\"100%\">\n");
-
-                while (reader.hasNextLine()) {
-
-                    if (!isInQuotes) {
-                        writer.append("<tr>\n");
-                    }
-
-                    char[] charArrayFromLine = reader.nextLine().toCharArray();
-
-                    for (int i = 0; i < charArrayFromLine.length; i++) {
-                        if (charArrayFromLine[i] == '"') {
-                            isInQuotes = !isInQuotes;
-                        } else if (charArrayFromLine[i] == delimiter) {
-                            resultString.append("");
-                        } else if (charArrayFromLine[i] == '<') {
-                            resultString.append("&lt;");
-                        } else if (charArrayFromLine[i] == '>') {
-                            resultString.append("&gt;");
-                        } else if (charArrayFromLine[i] == '&') {
-                            resultString.append("&amp;");
-                        } else {
-                            resultString.append(charArrayFromLine[i]);
-                        }
-
-                        if (!isInQuotes & (charArrayFromLine[i] == delimiter | (i == charArrayFromLine.length - 1 & !isInQuotes))) {
-                            writer.append("<td><center>").append(resultString.toString()).append("</center></td>");
-                            resultString.setLength(0);
-                        }
-
-                        if (isInQuotes & i == charArrayFromLine.length - 1) {
-                            resultString.append("<br>");
-                        }
-                    }
-
-                    if (!isInQuotes) {
-                        writer.append("</tr>\n");
-                    }
-                }
-
-                writer.append("</table>\n</body>\n</html>");
-                System.out.print("Done.");
-                return;
-            } catch (FileNotFoundException e) {
-                System.out.println("Исходный фаил не найден. Укажите корректный путь к CSV таблице:");
-                inputPath = new Scanner(System.in).nextLine();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                inputPath = new Scanner(System.in).nextLine();
+            if (!reader.hasNext()) {
+                throw new IllegalArgumentException("Исходный фаил пуст.");
             }
+
+            writer.append("<!DOCTYPE HTML>\n<html>\n");
+            writer.append("<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n</head>\n");
+            writer.append("<title>CSV</title>\n");
+            writer.append("<body>\n");
+            writer.append("<table border=\"1\" width=\"100%\">\n");
+
+            while (reader.hasNextLine()) {
+                if (!isInQuotes) {
+                    writer.append("<tr>\n");
+                }
+                char[] charArrayFromLine = reader.nextLine().toCharArray();
+
+                for (int i = 0; i < charArrayFromLine.length; i++) {
+                    if (charArrayFromLine[i] == delimiter) {
+                        resultString.append("");
+                    } else if (charArrayFromLine[i] == '"') {
+                        isInQuotes = !isInQuotes;
+                    } else if (charArrayFromLine[i] == '<') {
+                        resultString.append("&lt;");
+                    } else if (charArrayFromLine[i] == '>') {
+                        resultString.append("&gt;");
+                    } else if (charArrayFromLine[i] == '&') {
+                        resultString.append("&amp;");
+                    } else {
+                        resultString.append(charArrayFromLine[i]);
+                    }
+
+                    if (!isInQuotes & (charArrayFromLine[i] == delimiter | (i == charArrayFromLine.length - 1 & !isInQuotes))) {
+                        writer.append("\n<td><center>").append(resultString.toString()).append("</center></td>\n");
+                        resultString.setLength(0);
+                    }
+
+                    if (isInQuotes & i == charArrayFromLine.length - 1) {
+                        resultString.append("<br>");
+                    }
+                }
+                if (!isInQuotes) {
+                    writer.append("</tr>\n");
+                }
+            }
+            writer.append("\n</table>\n</body>\n</html>");
+            System.out.print("Done.");
+        } catch (FileNotFoundException e) {
+            System.out.println("Исходный фаил не найден.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
