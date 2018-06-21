fcd569a7b168ef576a250ff8c1ac5740d5e94439
Your Name <qeadws12>
2018-06-21 22:28:57 +0700

    21.06.2018

--- quadraticEquationJS/src/script.js
+++ quadraticEquationJS/src/script.js
@@ -1,53 +1,53 @@
-window.onload = function () {
+document.addEventListener("DOMContentLoaded", calculate);
 
-        document.getElementById("calculate_button").onclick = function () {
-            var a = document.getElementById("first_argument").value;
-            var b = document.getElementById("second_argument").value;
-            var c = document.getElementById("third_argument").value;
-            var equal = document.getElementById("equal").value;
+function calculate() {
+    document.getElementById("calculate_button").onclick = function () {
+        var a = document.getElementById("first_argument").value;
+        var b = document.getElementById("second_argument").value;
+        var c = document.getElementById("third_argument").value;
+        var equal = document.getElementById("equal").value;
 
-            if (isNaN(a) || isNaN(b) || isNaN(c) || isNaN(equal)) {
-                alert("Данные введены некорректно");
-            }
-            else {
-                var first_answer = document.getElementById("fist_answer");
-                var second__answer = document.getElementById("second_answer");
+        if (isNaN(a) || isNaN(b) || isNaN(c) || isNaN(equal)) {
+            alert("Данные введены некорректно");
+        }
+        else {
+            var firstAnswer = document.getElementById("fist_answer");
+            var secondAnswer = document.getElementById("second_answer");
 
-                var epsilon = 1.0e-10;
-                c -= equal;
+            var epsilon = 1.0e-10;
+            c -= equal;
 
-                var aIsZero = Math.abs(a) < epsilon;
-                var bIsZero = Math.abs(b) < epsilon;
-                var cIsZero = Math.abs(c) < epsilon;
+            var aIsZero = Math.abs(a) < epsilon;
+            var bIsZero = Math.abs(b) < epsilon;
+            var cIsZero = Math.abs(c) < epsilon;
 
-                if (aIsZero && bIsZero && cIsZero) {
-                    first_answer.value = "+∞";
-                    second__answer.value = "-∞";
-                } else if (aIsZero && bIsZero) {
-                    first_answer.value = "Решений нет";
-                    second__answer.value = "Решений нет";
-                } else if (aIsZero) {
-                    first_answer.value = (-c / b).toFixed(2);
-                    second__answer.value = "Один корень";
+            if (aIsZero && bIsZero && cIsZero) {
+                firstAnswer.value = "+∞";
+                secondAnswer.value = "-∞";
+            } else if (aIsZero && bIsZero) {
+                firstAnswer.value = "Решений нет";
+                secondAnswer.value = "Решений нет";
+            } else if (aIsZero) {
+                firstAnswer.value = (-c / b).toFixed(2);
+                secondAnswer.value = "Один корень";
+            } else {
+                var x1;
+                var x2;
+                var d = Math.pow(b, 2) - 4 * a * c;
+                if (d >= epsilon) {
+                    x1 = (-b + Math.sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.sqrt(d)) / (2 * a);
+                    firstAnswer.value = x1.toFixed(2);
+                    secondAnswer.value = x2.toFixed(2);
+                } else if (Math.abs(d) < epsilon) {
+                    x1 = -b / (2 * a);
+                    firstAnswer.value = x1.toFixed(2);
+                    secondAnswer.value = "Один корень"
                 } else {
-                    var x1;
-                    var x2;
-                    var d = Math.pow(b, 2) - 4 * a * c;
-                    if (d >= epsilon) {
-                        x1 = (-b + Math.sqrt(d)) / (2 * a);
-                        x2 = (-b - Math.sqrt(d)) / (2 * a);
-                        first_answer.value = x1.toFixed(2);
-                        second__answer.value = x2.toFixed(2);
-                    } else if (Math.abs(d) < epsilon) {
-                        x1 = -b / (2 * a);
-                        first_answer.value = x1.toFixed(2);
-                        second__answer.value = "Один корень"
-                    } else {
-                        first_answer.value = "Решений нет";
-                        second__answer.value = "Решений нет";
-                    }
+                    firstAnswer.value = "Решений нет";
+                    secondAnswer.value = "Решений нет";
                 }
             }
         }
-
-};
+    }
+}
